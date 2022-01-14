eef6ddea2a973cd887011f57c3c2dac17d9a86b5
Daniel Huerta <daniel.huerta@techmahindra>
2022-01-14 11:53:04 -0600

    changing rg error

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ data "azurerm_resource_group" "rg" {
 
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
-  resource_group_name = data.azurerm_resource_group.rg.example.name
-  location            = data.azurerm_resource_group.rg.example.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   address_space       = ["10.0.0.0/16"]
 }
